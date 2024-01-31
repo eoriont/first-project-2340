054ab1f7186c4d696fb849363c067db3368f6360
michaelhyi <michaelyi2004@gmail.com>
2024-01-31 11:21:11 -0500

    feat(user-story-4): implemented navigation between pages

--- app/src/main/java/com/example/firstproject/ui/userStory4/UserStory4Fragment.java
+++ app/src/main/java/com/example/firstproject/ui/userStory4/UserStory4Fragment.java
@@ -4,28 +4,48 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.fragment.NavHostFragment;
 
+import com.example.firstproject.R;
 import com.example.firstproject.databinding.FragmentUserStory4Binding;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 public class UserStory4Fragment extends Fragment {
-
     private FragmentUserStory4Binding binding;
+    private ListView view;
+    private ArrayList<String> list;
+    private ArrayAdapter<String> adapter;
+    private FloatingActionButton fab;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        UserStory4ViewModel userStory4ViewModel =
-                new ViewModelProvider(this).get(UserStory4ViewModel.class);
-
         binding = FragmentUserStory4Binding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textUserStory4;
-        userStory4ViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        view = binding.list;
+        adapter = new ArrayAdapter<String>(this.getContext(), R.layout.task, list);
+        view.setAdapter(adapter);
+
+        fab = binding.fab;
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavHostFragment.findNavController(UserStory4Fragment.this).navigate(
+                        UserStory4FragmentDirections.actionNavigationUserStory4ToNavigationCreateEditDeleteTask()
+                );
+            }
+        });
+
         return root;
     }
 
