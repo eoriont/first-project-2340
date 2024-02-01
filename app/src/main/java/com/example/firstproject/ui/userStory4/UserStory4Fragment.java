5a2226d638c1569e728a0bc60450c97cab3a4fac
michaelhyi <michaelyi2004@gmail.com>
2024-01-31 19:36:59 -0500

    feat(user-story-4): implemented CRUD features

--- app/src/main/java/com/example/firstproject/ui/userStory4/UserStory4Fragment.java
+++ app/src/main/java/com/example/firstproject/ui/userStory4/UserStory4Fragment.java
@@ -5,48 +5,103 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.firstproject.R;
 import com.example.firstproject.databinding.FragmentUserStory4Binding;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 
 public class UserStory4Fragment extends Fragment {
     private FragmentUserStory4Binding binding;
     private ListView view;
-    private ArrayList<String> list;
     private ArrayAdapter<String> adapter;
-    private FloatingActionButton fab;
+    private EditText idInput, taskInput;
+    private Button add, edit, delete;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentUserStory4Binding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        list = new ArrayList<>();
-        list.add("1");
-        list.add("2");
         view = binding.list;
-        adapter = new ArrayAdapter<String>(this.getContext(), R.layout.task, list);
+        adapter = new ArrayAdapter(this.getContext(), R.layout.task, new ArrayList<String>());
         view.setAdapter(adapter);
+        adapter.sort((a, b) -> b.compareTo(a));
 
-        fab = binding.fab;
-        fab.setOnClickListener(new View.OnClickListener() {
+        return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        idInput = binding.idInput;
+        taskInput = binding.taskInput;
+        add = binding.add;
+        edit = binding.edit;
+        delete = binding.delete;
+
+        add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                NavHostFragment.findNavController(UserStory4Fragment.this).navigate(
-                        UserStory4FragmentDirections.actionNavigationUserStory4ToNavigationCreateEditDeleteTask()
-                );
+                String id = idInput.getText().toString();
+                String task = taskInput.getText().toString();
+                adapter.add(String.format("#%s: %s", id, task));
+                adapter.sort((a, b) -> b.compareTo(a));
+
+                idInput.setText("");
+                taskInput.setText("");
             }
         });
 
-        return root;
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String id = idInput.getText().toString();
+                String task = taskInput.getText().toString();
+
+                for (int i = 0; i < adapter.getCount(); i++) {
+                    String curr = adapter.getItem(i);
+
+                    if (curr.substring(1, curr.indexOf(':')).equals(id)) {
+                        adapter.remove(curr);
+                        adapter.insert(String.format("#%s: %s", id, task), i);
+                        adapter.sort((a, b) -> b.compareTo(a));
+                        break;
+                    }
+                }
+
+                idInput.setText("");
+                taskInput.setText("");
+            }
+        });
+
+
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String id = idInput.getText().toString();
+
+                for (int i = 0; i < adapter.getCount(); i++) {
+                    String curr = adapter.getItem(i);
+
+                    if (curr.substring(1, curr.indexOf(':')).equals(id)) {
+                        adapter.remove(curr);
+                        break;
+                    }
+                }
+
+                idInput.setText("");
+                taskInput.setText("");
+            }
+        });
     }
 
     @Override
